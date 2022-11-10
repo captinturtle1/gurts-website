d25b973538457ab73808a7a84ba84409a566c897
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-11-10 11:32:45 -0800

    footer & nav mobile support

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="relative h-[400px] z-[2] bg-zinc-700 flex">
+        <div className="relative h-[500px] z-[2] bg-zinc-700 flex">
             <div className="z-[3] mx-auto mt-72 font-semibold text-xl">YogurtVerse © 2022 ALL RIGHTS RESERVED.</div>
         </div>
     )
